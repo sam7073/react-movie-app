bf2ddde933dccd012d5ff80d9d97edd5d600f5d0
unknown <sam7073@naver.com>
2020-07-26 22:49:42 +0900

    Updates

---
+++ precache-manifest.1ad037c634916c65132efcfd90d52b71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40c16c121f0ce5754b837a70d84540c",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "b5f95d5142f41d4bedd0",
+    "url": "/react-movie-app/static/js/2.2f81e473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.2f81e473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71da718bc83238c13e12",
+    "url": "/react-movie-app/static/js/main.f8140f47.chunk.js"
+  },
+  {
+    "revision": "234beef1cb79a5d03d2b",
+    "url": "/react-movie-app/static/js/runtime-main.78514abc.js"
+  }
+]);
